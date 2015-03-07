a26771946a6da71d707ecfb3e62c8318c63e7469
Youssef <youssefhad2@gmail.com>
2015-03-07 12:29:59 +0000

    integration de video

--- index.php
+++ index.php
@@ -169,6 +169,11 @@
 </head>
 <body>
 
+    <!-- Video -->
+    <video id="big-video-vid_html5_api" class="vjs-tech" preload="auto" data-setup="{}" autoplay="" webkit-playsinline="" poster="null" src="video/1" style="position: fixed; /*width: 1203.55555555556px;*/height:1000px" muted loop></video>
+
+    <!-- <video id="big-video-vid_html5_api" class="vjs-tech" preload="auto" data-setup="{}" autoplay="" webkit-playsinline="" poster="null" src="video/1" style="position: fixed; /*width: 1203.55555555556px;*/width: 100%;height:100vh" muted loop></video> -->
+
 	<!-- Loader -->
 	<div id="loading">
     	<img src="images/loader.gif" alt="Website Loader"/>
@@ -1379,50 +1384,50 @@
     <script type="text/javascript" src="javascripts/jquery-css-transform.js"></script>
     
     <!-- Supersized JS Files -->
-	<script type="text/javascript" src="javascripts/supersized.3.2.7.min.js"></script>
-	<script type="text/javascript" src="javascripts/supersized.shutter.min.js"></script>
+	<!-- <script type="text/javascript" src="javascripts/supersized.3.2.7.min.js"></script>
+	<script type="text/javascript" src="javascripts/supersized.shutter.min.js"></script> -->
 	
 	<script type="text/javascript">
-		jQuery(function($){
-			$.supersized({
-				// Functionality
-				slideshow               :   1,			// Slideshow on/off
-				autoplay				:	1,			// Slideshow starts playing automatically
-				start_slide             :   1,			// Start slide (0 is random)
-				stop_loop				:	0,			// Pauses slideshow on last slide
-				random					: 	0,			// Randomize slide order (Ignores start slide)
-				slide_interval          :   3000,		// Length between transitions
-				transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-				transition_speed		:	1500,		// Speed of transition
-				new_window				:	1,			// Image links open in new window/tab
-				pause_hover             :   0,			// Pause slideshow on hover
-				keyboard_nav            :   1,			// Keyboard navigation on/off
-				performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-				image_protect			:	1,			// Disables image dragging and right click with Javascript								   
-				// Size & Position						   
-				min_width		        :   0,			// Min width allowed (in pixels)
-				min_height		        :   0,			// Min height allowed (in pixels)
-				vertical_center         :   1,			// Vertically center background
-				horizontal_center       :   1,			// Horizontally center background
-				fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
-				fit_portrait         	:   1,			// Portrait images will not exceed browser height
-				fit_landscape			:   0,			// Landscape images will not exceed browser width
-				// Components							
-				slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
-				thumb_links				:	1,			// Individual thumb links for each slide
-				thumbnail_navigation    :   0,			// Thumbnail navigation
-				slides 					:  	[			// Slideshow Images
-												{image : 'images/homepage-slideshow/1.jpg', title : 'Dreamer - Responsive One Page Parallax Template'},
-												{image : 'images/homepage-slideshow/2.jpg', title : 'Dreamer - Responsive One Page Parallax Template'},
-												{image : 'images/homepage-slideshow/3.jpg', title : 'Dreamer - Responsive One Page Parallax Template'}
-											],
+		// jQuery(function($){
+		// 	$.supersized({
+		// 		// Functionality
+		// 		slideshow               :   1,			// Slideshow on/off
+		// 		autoplay				:	1,			// Slideshow starts playing automatically
+		// 		start_slide             :   1,			// Start slide (0 is random)
+		// 		stop_loop				:	0,			// Pauses slideshow on last slide
+		// 		random					: 	0,			// Randomize slide order (Ignores start slide)
+		// 		slide_interval          :   3000,		// Length between transitions
+		// 		transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+		// 		transition_speed		:	1500,		// Speed of transition
+		// 		new_window				:	1,			// Image links open in new window/tab
+		// 		pause_hover             :   0,			// Pause slideshow on hover
+		// 		keyboard_nav            :   1,			// Keyboard navigation on/off
+		// 		performance				:	1,			// 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+		// 		image_protect			:	1,			// Disables image dragging and right click with Javascript								   
+		// 		// Size & Position						   
+		// 		min_width		        :   0,			// Min width allowed (in pixels)
+		// 		min_height		        :   0,			// Min height allowed (in pixels)
+		// 		vertical_center         :   1,			// Vertically center background
+		// 		horizontal_center       :   1,			// Horizontally center background
+		// 		fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
+		// 		fit_portrait         	:   1,			// Portrait images will not exceed browser height
+		// 		fit_landscape			:   0,			// Landscape images will not exceed browser width
+		// 		// Components							
+		// 		slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
+		// 		thumb_links				:	1,			// Individual thumb links for each slide
+		// 		thumbnail_navigation    :   0,			// Thumbnail navigation
+		// 		slides 					:  	[			// Slideshow Images
+		// 										{image : 'images/homepage-slideshow/1.jpg', title : 'Dreamer - Responsive One Page Parallax Template'},
+		// 										{image : 'images/homepage-slideshow/2.jpg', title : 'Dreamer - Responsive One Page Parallax Template'},
+		// 										{image : 'images/homepage-slideshow/3.jpg', title : 'Dreamer - Responsive One Page Parallax Template'}
+		// 									],
 												
-				// Theme Options			   
-				progress_bar			:	1,			// Timer for each slide							
-				mouse_scrub				:	0
+		// 		// Theme Options			   
+		// 		progress_bar			:	1,			// Timer for each slide							
+		// 		mouse_scrub				:	0
 					
-			});
-		});
+		// 	});
+		// });
     </script>
     
     <!-- Initialize JS Plugins -->
